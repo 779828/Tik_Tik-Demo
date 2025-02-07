852b9adc66717efd3e072e7162c36127d84cedfd
Shashank <shashankbmusale07@gmail.com>
2025-02-07 13:12:26 +0530

    add grid table

--- src/utils/UsersTable.jsx
+++ src/utils/UsersTable.jsx
@@ -1,31 +1,24 @@
 import React, { useEffect, useState } from "react";
+import { DataGrid } from "@mui/x-data-grid";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchUsers, deleteUser, editUser } from "../features/userSlice";
+import { fetchUsers, deleteUser, editUser } from "../features/userSlice.js";
 import {
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  Paper,
   Button,
-  TablePagination,
-  Dialog,
-  DialogTitle,
-  DialogContent,
-  DialogActions,
   TextField,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
 } from "@mui/material";
 
-const UserTable = () => {
+const UsersTable = () => {
   const dispatch = useDispatch();
   const { users, loading } = useSelector((state) => state.users);
 
-  const [page, setPage] = useState(0);
-  const [rowsPerPage, setRowsPerPage] = useState(10);
   const [open, setOpen] = useState(false);
   const [selectedUser, setSelectedUser] = useState(null);
+  const [editedName, setEditedName] = useState("");
+  const [editedEmail, setEditedEmail] = useState("");
 
   useEffect(() => {
     dispatch(fetchUsers());
@@ -35,134 +28,83 @@ const UserTable = () => {
     dispatch(deleteUser(id));
   };
 
-  const handleEdit = (user) => {
+  const handleEditClick = (user) => {
     setSelectedUser(user);
+    setEditedName(user.name);
+    setEditedEmail(user.email);
     setOpen(true);
   };
 
-  const handleSave = () => {
-    dispatch(editUser(selectedUser));
+  const handleEditSave = () => {
+    dispatch(
+      editUser({ ...selectedUser, name: editedName, email: editedEmail })
+    );
     setOpen(false);
   };
 
-  const handleChangePage = (event, newPage) => {
-    setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = (event) => {
-    setRowsPerPage(parseInt(event.target.value, 10));
-    setPage(0);
-  };
-
-  if (loading) return <p>Loading...</p>;
+  const columns = [
+    { field: "id", headerName: "ID", width: 90 },
+    { field: "name", headerName: "Name", flex: 1 },
+    { field: "email", headerName: "Email", flex: 1 },
+    {
+      field: "actions",
+      headerName: "Actions",
+      width: 200,
+      renderCell: (params) => (
+        <>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => handleEditClick(params.row)}
+          >
+            Edit
+          </Button>
+          <Button
+            variant="contained"
+            color="secondary"
+            onClick={() => handleDelete(params.row.id)}
+            style={{ marginLeft: 8 }}
+          >
+            Delete
+          </Button>
+        </>
+      ),
+    },
+  ];
 
   return (
-    <Paper sx={{ width: "100%", overflow: "hidden" }}>
-      <TableContainer
-        sx={{
-          overflowX: "auto",
-          msOverflowStyle: "none",
-          scrollbarWidth: "none",
-        }}
-      >
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>ID</TableCell>
-              <TableCell>First Name</TableCell>
-              <TableCell>Last Name</TableCell>
-              <TableCell>Email</TableCell>
-              <TableCell>Actions</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {users
-              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-              .map((user) => (
-                <TableRow key={user.id}>
-                  <TableCell>{user.id}</TableCell>
-                  <TableCell>{user.name.split(" ")[0]}</TableCell>
-                  <TableCell>{user.name.split(" ")[1] || ""}</TableCell>
-                  <TableCell>{user.email}</TableCell>
-                  <TableCell>
-                    <Button color="primary" onClick={() => handleEdit(user)}>
-                      Edit
-                    </Button>
-                    <Button
-                      color="secondary"
-                      onClick={() => handleDelete(user.id)}
-                    >
-                      Delete
-                    </Button>
-                  </TableCell>
-                </TableRow>
-              ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-
-      <TablePagination
-        rowsPerPageOptions={[5, 10, 15]}
-        component="div"
-        count={users.length}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onPageChange={handleChangePage}
-        onRowsPerPageChange={handleChangeRowsPerPage}
-        sx={{
-          overflowX: "auto",
-          msOverflowStyle: "none",
-          scrollbarWidth: "none",
-        }}
-      />
-
+    <div style={{ height: 700, width: "100%" }}>
+      <DataGrid rows={users} columns={columns} loading={loading} pageSize={5} />
       {/* Edit Dialog */}
       <Dialog open={open} onClose={() => setOpen(false)}>
         <DialogTitle>Edit User</DialogTitle>
         <DialogContent>
           <TextField
-            label="First Name"
+            label="Name"
             fullWidth
-            value={selectedUser?.name.split(" ")[0] || ""}
-            onChange={(e) =>
-              setSelectedUser({
-                ...selectedUser,
-                name: `${e.target.value} ${selectedUser?.name.split(" ")[1] || ""}`,
-              })
-            }
-            sx={{ my: 1 }}
-          />
-          <TextField
-            label="Last Name"
-            fullWidth
-            value={selectedUser?.name.split(" ")[1] || ""}
-            onChange={(e) =>
-              setSelectedUser({
-                ...selectedUser,
-                name: `${selectedUser?.name.split(" ")[0] || ""} ${e.target.value}`,
-              })
-            }
-            sx={{ my: 1 }}
+            margin="dense"
+            value={editedName}
+            onChange={(e) => setEditedName(e.target.value)}
           />
           <TextField
             label="Email"
             fullWidth
-            value={selectedUser?.email || ""}
-            onChange={(e) =>
-              setSelectedUser({ ...selectedUser, email: e.target.value })
-            }
-            sx={{ my: 1 }}
+            margin="dense"
+            value={editedEmail}
+            onChange={(e) => setEditedEmail(e.target.value)}
           />
         </DialogContent>
         <DialogActions>
-          <Button onClick={() => setOpen(false)}>Cancel</Button>
-          <Button onClick={handleSave} color="primary">
+          <Button onClick={() => setOpen(false)} color="secondary">
+            Cancel
+          </Button>
+          <Button onClick={handleEditSave} color="primary">
             Save
           </Button>
         </DialogActions>
       </Dialog>
-    </Paper>
+    </div>
   );
 };
 
-export default UserTable;
+export default UsersTable;
